7a4b95744f35ee5b804520879a6ddf8df403f999
Matias Jesus Alicata <matiasalicata@gmail.com>
2022-09-29 01:10:33 -0300

    Add a new file

---
+++ api/consultar.php
@@ -0,0 +1,15 @@
+<?php
+//http://localhost/sistema_php_2022/api/index.php?nombre=Matias&apellido=messi&email=asdas@afsa.com&telefono=125151&detalle=asdasd    
+    $conexion = new mysqli("localhost","root","123456","sistema", "8111") or die("not connected".mysqli_connect_error());
+   
+        $sql="SELECT * FROM `clientes`";
+        $result=mysqli_query($conexion,$sql);
+    
+        $clientes = array();
+        while ($fila = mysqli_fetch_array($result)) {
+          array_push($clientes, $fila);
+        }
+        echo json_encode($clientes);
+
+    
+?>
